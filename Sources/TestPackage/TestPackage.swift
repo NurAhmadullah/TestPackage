3b232fc24888576767081700c4aaf2cfeaf1845e
Nur Ahmadullah <nur.ahmadullah@brainstation-23.com>
2025-04-18 23:22:41 +0600

    added math operation

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -1,2 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+
+
+import Foundation
+
+public class MathOperation {
+    
+    public func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    
+    public func multiply(_ a: Int, _ b: Int) -> Int {
+        return a * b
+    }
+}
